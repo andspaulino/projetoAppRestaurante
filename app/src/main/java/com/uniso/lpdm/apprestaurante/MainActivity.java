c87d4c69a8137215e0a976f886c422c0e2c03fed
Anderson Paulino <andspaulino@gmail.com>
2020-09-08 14:39:02 -0300

    Alterações feitas de acordo as atividades

--- app/src/main/java/com/uniso/lpdm/apprestaurante/MainActivity.java
+++ app/src/main/java/com/uniso/lpdm/apprestaurante/MainActivity.java
@@ -1,5 +1,8 @@
 package com.uniso.lpdm.apprestaurante;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvM = (TextView)findViewById(R.id.tvMessage);
+        final Button btFP = (Button)findViewById(R.id.bFazerPedido);
+        final Button btTH = (Button)findViewById(R.id.bTotalHoras);
+
+        btFP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvM.setText("Registrando Pedido");
+            }
+        });
+
+        btTH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvM.setText("Total de Horas");
+            }
+        });
     }
 }
